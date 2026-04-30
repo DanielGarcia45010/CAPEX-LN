85e48860f8c5303eecbdb6ccb9305b2eaca92af8
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2026-04-30 22:10:58 +0000

    correcion path

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ import pickle
 from scipy.spatial import cKDTree
 
 # 🔥 ARCHIVO EN DESCARGAS
-INPUT = r"C:\Users\1872643\Downloads\test.json"
+INPUT = str(Path("C:/Users/1872643/Downloads/test.json"))
 OUTPUT = "data/index.pkl"
 
 os.makedirs("data", exist_ok=True)
