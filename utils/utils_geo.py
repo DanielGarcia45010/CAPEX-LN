bf6e4ced14699b26296c73c2e507746e2aeda69f
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2026-05-05 22:33:16 +0000

    coreccion utils

--- utils/utils_geo.py
+++ utils/utils_geo.py
@@ -1,12 +1,13 @@
 import math
-from functools import lru_cache
 
 R = 6371000
 
 
-@lru_cache(maxsize=100000)
 def haversine(lon1, lat1, lon2, lat2):
-    phi1, phi2 = math.radians(lat1), math.radians(lat2)
+
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+
     dphi = math.radians(lat2 - lat1)
     dlambda = math.radians(lon2 - lon1)
 
