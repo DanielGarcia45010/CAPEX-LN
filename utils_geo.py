83b0acf9ab749ee266c69ab9cf3f5c71b545888e
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2026-05-05 21:43:06 +0000

    correccion utils

--- utils_geo.py
+++ utils_geo.py
@@ -1,23 +1,19 @@
 import math
+from functools import lru_cache
 
+R = 6371000
+
+
+@lru_cache(maxsize=100000)
 def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calcula distancia en metros entre dos puntos geográficos.
-    """
-
-    R = 6371000  # radio tierra en metros
-
-    phi1 = math.radians(lat1)
-    phi2 = math.radians(lat2)
-
+    phi1, phi2 = math.radians(lat1), math.radians(lat2)
     dphi = math.radians(lat2 - lat1)
     dlambda = math.radians(lon2 - lon1)
 
     a = (
-        math.sin(dphi / 2) ** 2
-        + math.cos(phi1) * math.cos(phi2) * math.sin(dlambda / 2) ** 2
+        math.sin(dphi / 2) ** 2 +
+        math.cos(phi1) * math.cos(phi2) *
+        math.sin(dlambda / 2) ** 2
     )
 
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    return R * c
+    return R * (2 * math.atan2(math.sqrt(a), math.sqrt(1 - a)))
