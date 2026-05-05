82387d47ba8ffa6ed0cd768cd56cca8d742ccca9
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2026-05-05 20:55:14 +0000

    agregar capex

---
+++ capex_scoring.py
@@ -0,0 +1,15 @@
+import math
+
+def capex_score(distance_m, density, presence_bonus):
+
+    # normalización simple industrial
+
+    distance_score = 1 / (1 + distance_m)
+
+    density_score = math.log(1 + density)
+
+    return (
+        0.5 * distance_score +
+        0.3 * density_score +
+        0.2 * presence_bonus
+    )
