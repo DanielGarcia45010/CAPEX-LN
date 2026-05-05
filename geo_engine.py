98a85866622cee1b7234ba24af8d253e9bb74cda
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2026-05-05 22:01:50 +0000

    mejora motor

--- geo_engine.py
+++ geo_engine.py
@@ -1,19 +1,41 @@
-import pickle
+import h3
+from collections import defaultdict
+
 
 class GeoEngine:
 
-    def __init__(self, path="data/index.pkl"):
+    def __init__(self, resolution=9):
+        self.resolution = resolution
+        self.index = defaultdict(list)
+        self.centroids = []
 
-        with open(path, "rb") as f:
-            self.tree, self.geom_index = pickle.load(f)
+    def build(self, geometries):
 
-    def query(self, lon, lat, k=25):
+        for i, geom in enumerate(geometries):
 
-        dist, idx = self.tree.query([lon, lat], k=k)
+            try:
+                c = geom.centroid
+                self.centroids.append((c.x, c.y))
 
+                h = h3.latlng_to_cell(c.y, c.x, self.resolution)
+                self.index[h].append(i)
+
+            except:
+                continue
+
+    def query(self, lon, lat, max_k=6, min_results=50):
+
+        center = h3.latlng_to_cell(lat, lon, self.resolution)
         results = []
 
-        for d, i in zip(dist, idx):
-            results.append((d * 111320, self.geom_index[i]))
+        for k in range(1, max_k + 1):
+
+            for h in h3.grid_disk(center, k):
+                if h in self.index:
+                    for idx in self.index[h]:
+                        results.append((h, idx))
+
+            if len(results) >= min_results:
+                break
 
         return results
