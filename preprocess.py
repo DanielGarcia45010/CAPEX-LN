73f49be09a5db4120b6e31d482f543f98f05c996
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2026-05-05 21:45:23 +0000

    correccion preprocesamiento

--- preprocess.py
+++ preprocess.py
@@ -1,52 +1,36 @@
 import json
 import os
-from pathlib import Path
-from shapely.geometry import shape
 import numpy as np
 import pickle
+from shapely.geometry import shape
 from scipy.spatial import cKDTree
 
-# 🔥 ARCHIVO EN DESCARGAS
 INPUT = "test.json"
 OUTPUT = "data/index.pkl"
 
 os.makedirs("data", exist_ok=True)
 
-print(f"Loading file from: {INPUT}")
-
-if not os.path.exists(INPUT):
-    raise FileNotFoundError(f"No se encontró el archivo en {INPUT}")
-
 with open(INPUT, "r", encoding="utf-8") as f:
     data = json.load(f)
 
-features = data["features"]
-
 points = []
 index = []
+centroids = []
 
-print(f"Features: {len(features)}")
-
-for i, f in enumerate(features):
+for i, f in enumerate(data["features"]):
 
     try:
         geom = shape(f["geometry"])
         c = geom.centroid
 
         points.append([c.x, c.y])
+        centroids.append((c.x, c.y))
         index.append(i)
 
     except:
         continue
 
-    if i % 500 == 0:
-        print(f"Processed {i}/{len(features)}")
-
-print("Building KDTree...")
-
 tree = cKDTree(np.array(points))
 
 with open(OUTPUT, "wb") as f:
-    pickle.dump((tree, index), f)
-
-print("DONE ✅ index saved:", OUTPUT)
+    pickle.dump((tree, index, centroids), f)
